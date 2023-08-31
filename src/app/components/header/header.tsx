39e70463d5a6022d7e6bb8ac7bf7273964e5a485
João Oliveira <joaooliveira1999@outlook.com>
2023-08-30 21:20:27 -0300

    feat: add initial html structure and header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import "./header.scss";
+
+export function Header() {
+  return (
+    <div className="header">
+      <div>
+        <h1>Olá, Eu sou o João 👋 </h1>
+        <h2>Engenheiro de software</h2>
+      </div>
+      <Image src="/me2.jpeg" alt="Perfil" width={325} height={310} priority />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
-import Image from "next/image";
+import { Header } from "./components/header/header";
+
+import "./styles/home.scss";
 
 export default function Home() {
   return (
-    <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-      />
+    <main className="container">
+      <Header />
+      <div className="experience">
+        <h3>Experiencia</h3>
+        <p>Tenho 1 ano de experiencia trabalhando com .Net como freelancer</p>
+        <div className="experience-time"></div>
+        <div className="infos">
+          <h3>Linguas</h3>
+          <div className="languages-info">
+            <span> EN - Intermediario</span>
+            <span> PT-BR - Nativo</span>
+          </div>
+          <h3>Formação</h3>
+          <div className="educational-info">
+            <span>🎓</span>
+            <span>
+              Engenharia da computação - Universidade Centro Verde Maringa
+            </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social"></div>
+          <button> Entre em contato</button>
+        </div>
+      </div>
     </main>
   );
 }
