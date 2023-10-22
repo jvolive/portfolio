4edf51d4e539bdbe4441672b0e8a4f6b9876f6e0
João Oliveira <joaooliveira1999@outlook.com>
2023-10-22 01:38:47 -0300

    start experience

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,9 @@
+export function Experience() {
+  return (
+    <div className="experience">
+      <h3>Experiencia</h3>
+      <p>Tenho 1 ano de experiencia trabalhando com .Net como freelancer</p>
+      <div className="experience-time"></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,24 @@ export default function Home() {
   return (
     <main className="container">
       <Header />
-      <div className="experience">
-        <h3>Experiencia</h3>
-        <p>Tenho 1 ano de experiencia trabalhando com .Net como freelancer</p>
-        <div className="experience-time"></div>
-        <div className="infos">
-          <h3>Linguas</h3>
-          <div className="languages-info">
-            <span> EN - Intermediario</span>
-            <span> PT-BR - Nativo</span>
-          </div>
-          <h3>Formação</h3>
-          <div className="educational-info">
-            <span>🎓</span>
-            <span>
-              Engenharia da computação - Universidade Centro Verde Maringa
-            </span>
-          </div>
+
+      <div className="infos">
+        <h3>Linguas</h3>
+        <div className="languages-info">
+          <span> EN - Intermediario</span>
+          <span> PT-BR - Nativo</span>
         </div>
-        <div className="buttons">
-          <div className="social"></div>
-          <button> Entre em contato</button>
+        <h3>Formação</h3>
+        <div className="educational-info">
+          <span>🎓</span>
+          <span>
+            Engenharia da computação - Universidade Centro Verde Maringa
+          </span>
         </div>
+      </div>
+      <div className="buttons">
+        <div className="social"></div>
+        <button> Entre em contato</button>
       </div>
     </main>
   );
