b3e4037fcbef1a3e24b90a7414b5ede1de6f5162
João Oliveira <joaooliveira1999@outlook.com>
2023-10-23 00:06:31 -0300

    finish tutorial

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SectionTitle } from "../sectionTitle/section-title";
 
 import "./experience.scss";
@@ -7,7 +8,44 @@ export function Experience() {
     <div className="experience">
       <SectionTitle text="Experiencia" />
       <p>Tenho 1 ano de experiencia trabalhando com .Net como freelancer</p>
-      <div className="experience-time"></div>
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image
+            src="/csharp.png"
+            alt="CSharp"
+            width={45}
+            height={45}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-2">
+              <span>2 anos</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/Js.png"
+            alt="JavaScript"
+            width={45}
+            height={45}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+              <span>1 anos</span>
+            </div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image src="/React.png" alt="React" width={45} height={45} priority />
+          <div className="experience-unit">
+            <div className="experience-measure measure-1">
+              <span>1 anos</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
