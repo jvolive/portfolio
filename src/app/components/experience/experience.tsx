deef20e81266df15b7f739971943831e7635c140
João Oliveira <contato.jvolive@gmail.com>
2025-04-02 10:45:25 -0300

    feat: update experiences

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -8,63 +8,30 @@ export function Experience() {
     <div className="experience">
       <SectionTitle text="Experiencia" />
       <p>
-        Sou desenvolvedor Full Stack com C# trabalhando com .NET, ASP.NET e
-        TypeScript com ReactJS.
-        <p>Também com conhecimento em PostgreSQL e Azure.</p>
+        Sou desenvolvedor FrontEnd trabalhando com Flutter, ReactNative.
+        <p>Também com conhecimento em HTML/CSS | SQL | APIs REST | .NET</p>
       </p>
       <div className="experience-time">
-        <div className="csharp">
+        <div className="dart">
           <div className="experience-language">
             <Image
-              src="/csharp.png"
-              alt="CSharp"
-              width={45}
-              height={45}
-              priority
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg"
+              alt="Flutter"
+              width="45"
+              height="45"
             />
             <div className="experience-unit">
-              <div className="experience-measure measure-3">
-                <span>3 anos</span>
+              <div className="experience-measure measure-1">
+                <span>1 ano</span>
               </div>
             </div>
           </div>
         </div>
-        <div className="dotnet">
-          <div className="experience-language">
-            <Image
-              src="/dotnet.png"
-              alt=".net"
-              width={45}
-              height={45}
-              priority
-            />
-            <div className="experience-unit">
-              <div className="experience-measure measure-3">
-                <span>3 anos</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="typescript">
-          <div className="experience-language">
-            <Image
-              src="/Ts.png"
-              alt="TypeScript"
-              width={65}
-              height={45}
-              priority
-            />
-            <div className="experience-unit">
-              <div className="experience-measure measure-2">
-                <span>2 ano</span>
-              </div>
-            </div>
-          </div>
-        </div>
+
         <div className="react">
           <div className="experience-language">
             <Image
-              src="/React.png"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
               alt="React"
               width={45}
               height={45}
@@ -77,6 +44,57 @@ export function Experience() {
             </div>
           </div>
         </div>
+
+        <div className="typescript">
+          <div className="experience-language">
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+              alt="TypeScript"
+              width={65}
+              height={45}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-2">
+                <span>2 ano</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="dotnet">
+          <div className="experience-language">
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg"
+              alt=".net"
+              width={45}
+              height={45}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-3">
+                <span>3 anos</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="dotnet">
+          <div className="experience-language">
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg"
+              alt=".net"
+              width={45}
+              height={45}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-3">
+                <span>3 anos</span>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
